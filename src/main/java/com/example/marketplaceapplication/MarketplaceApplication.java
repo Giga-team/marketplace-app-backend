6b1e291409d1b72b72b0c15e506e2d5d604712d4
Roman Prokopchuk <71777440+ram333n@users.noreply.github.com>
2023-01-03 21:20:00 +0200

    Configure style, add plugin

--- src/main/java/com/example/marketplaceapplication/MarketplaceApplication.java
+++ src/main/java/com/example/marketplaceapplication/MarketplaceApplication.java
@@ -3,11 +3,14 @@ package com.example.marketplaceapplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main application.
+ */
 @SpringBootApplication
 public class MarketplaceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MarketplaceApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(MarketplaceApplication.class, args);
+  }
 
 }
